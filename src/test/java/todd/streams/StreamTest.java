6d042541919c233c246f47947b5c969a5c7de8e5
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2017-02-08 21:07:58 -0500

    create first test

---
+++ src/test/java/todd/streams/StreamTest.java
@@ -0,0 +1,13 @@
+package todd.streams;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class StreamTest {
+
+	@Test
+	public void convertListToMap() {
+		Assertions.assertThat("foo").startsWith("f");
+	}
+
+}
